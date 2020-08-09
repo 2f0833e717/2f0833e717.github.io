a56e9250cde557d9a580ec397c5565019bdc2f76
2f0833e717 <skinoshita202001082135@gmail.com>
2020-08-10 02:11:33 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,4 @@
 set -e
 
 npm run build
-
-cd dist
-
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# https://2f0833e717.github.io
-git push -f git@github.com:2f0833e717/2f0833e717.github.io.git master
-
-# https://2f0833e717.github.io/<REPO>
-git push -f git@github.com:2f0833e717/<REPO>.git master:gh-pages
-
-# deploy force.
-npx gh-pages -d dist -b master
-
-cd -
+npm run deploy
